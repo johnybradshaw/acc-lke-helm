4e4378e5afd2f8accfa4bc842b23b1685928db71
John <johnybradshaw@users.noreply.github.com>
2024-01-03 19:36:34 +0000

    Change to providers.tf

--- provider.tf
+++ provider.tf
@@ -91,4 +91,7 @@ provider "kubectl" {
     cluster_ca_certificate = base64decode(
         local.kube_config_map.clusters[0].cluster["certificate-authority-data"]
     )
+
+    load_config_file = false # Disables local loading of the KUBECONFIG
+    apply_retry_count = 15 # Allows kubernetes commands to be retried
 }
