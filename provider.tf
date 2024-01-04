76bfc3912704b37bbf209868ca4c81f80fb1704f
John <johnybradshaw@users.noreply.github.com>
2024-01-04 10:35:42 +0000

    Renable provider flag

--- provider.tf
+++ provider.tf
@@ -92,5 +92,6 @@ provider "kubectl" {
         local.kube_config_map.clusters[0].cluster["certificate-authority-data"]
     )
 
+    load_config_file = false # Disables local loading of the KUBECONFIG
     apply_retry_count = 15 # Allows kubernetes commands to be retried
 }
